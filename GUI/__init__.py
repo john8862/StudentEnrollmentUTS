9f66d29a0370cc445ffcd613343cc3329166d32b
john8862 <40332568+john8862@users.noreply.github.com>
2023-10-27 23:50:40 +1100

    BREAKING CHANGE: New version of GUI finalised
    On branch new-version
    The branch is up to date with 'origin/new-version'.
    
    - Archieve old version of GUI
    - Finalise version of GUI
    - Increase version to 1.0.0
    - Change the name of old GUI folder to EOL
    - Remove unused code in each file
    - Create new __init__.py file
    
    Changes to be committed:
    	renamed:    GUI/ForgotPassword.py -> EOL/ForgotPassword.py
    	renamed:    GUI/SignIn.py -> EOL/SignIn.py
    	renamed:    GUI/SignUp.py -> EOL/SignUp.py
    	new file:   EOL/__init__.py
    	renamed:    GUI/Image/background.jpg -> EOL/background.jpg
    	renamed:    GUI/Image/bg.jpg -> EOL/bg.jpg
    	renamed:    GUI/Image/closeye.png -> EOL/closeye.png
    	renamed:    GUI/Image/email-icon.png -> EOL/email-icon.png
    	renamed:    GUI/Image/openeye.png -> EOL/openeye.png
    	renamed:    GUI/Image/password-icon.png -> EOL/password-icon.png
    	renamed:    GUI/Image/side-img.png -> EOL/side-img.png
    	modified:   GUI/ForgotPasswordNew.py
    	modified:   GUI/SignInNew.py
    	modified:   GUI/SignUpNew.py
    	modified:   GUI/StudentMainPage.py
    	modified:   GUI/Validation.py
    	modified:   GUI/__init__.py
    	modified:   students.data

--- GUI/__init__.py
+++
@@ -1,7 +0,0 @@
-import platform
-
-def set_button_cursor(button):
-    if platform.system() == "Windows":
-        button.config(cursor="hand2")
-    elif platform.system() == "Darwin":
-        button.config(cursor="pointinghand")
